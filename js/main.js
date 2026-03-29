a986fdbeac785e70c5d30bb5dffff5c8bb47e7cc
HUNTER SCOUTI <Zilas2004@gmail.com>
2026-03-29 12:25:35 +0200

    Fullscale update and added proper JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
+const languageIcons = [
+	{
+		name: "C++",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg"
+	},
+	{
+		name: "Java",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"
+	},
+	{
+		name: "Delphi",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/delphi/delphi-original.svg"
+	},
+	{
+		name: "Python",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+	},
+	{
+		name: "SQLite",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original.svg"
+	},
+	{
+		name: "Rust",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rust/rust-original.svg"
+	},
+	{
+		name: "JavaScript",
+		src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+	}
+];
+
+function renderLanguageIcons() {
+	const iconGrid = document.getElementById("lang-grid");
+	if (!iconGrid) {
+		return;
+	}
+
+	const iconMarkup = languageIcons
+		.map(
+			(icon) => {
+				const rustClass = icon.name === "Rust" ? " is-rust" : "";
+				return `<li class="lang-grid-item${rustClass}" title="${icon.name}"><img src="${icon.src}" alt="${icon.name} logo" loading="lazy" referrerpolicy="no-referrer"></li>`;
+			}
+		)
+		.join("");
+
+	iconGrid.innerHTML = iconMarkup;
+}
+
+renderLanguageIcons();
  
